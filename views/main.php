26cde72e5427007ac3781a372dad936251a02ad3
Georges Haddad <prismosoft@gmail.com>
2021-03-25 14:17:31 -0400

    Customize migration form Dynamic form shortcode by plugin id

--- views/main.php
+++ views/main.php
@@ -1,38 +1,103 @@
-<form method="post">
+<script>
+    (function( $ ) {
+        $(function() {
+            $('#xt-plugins').on('change', function() {
 
-  <?php do_action('ctf_success_message', $atts['form_success_message']); ?>
+                $(this).closest('.control').find('.select').removeClass('is-info').addClass('is-loading');
 
-	<?php if ($messages = $this->messages->get_error_messages()) : ?>
-    <p style="border: solid 1px #dd98a9;
-    border-radius: 3px;
-    padding: 12px;
-    background-color: #fff2f0;">
-		<?php echo implode('<br>', $messages); ?>
-    </p>
-	<?php endif; ?>
+                var url = '<?php echo home_url('/codecanyon-to-xt-migration'); ?>' + '?id='+$(this).val();
 
-  <p>
-    <label for="np-ctf-panel-email">
-		<?php echo $atts['form_email_label']; ?>:
-      <input id="np-ctf-panel-email" class="form-control form-control-lg" type="email" name="email" placeholder="youremail@company.com">
-    </label>
-  </p>
+                if(location.href  !== url) {
+                    location.href = url;
+                }
+            });
 
-  <p>
-    <label for="np-ctf-panel-license-key">
-		<?php echo $atts['form_license_label']; ?>:
-      <input id="np-ctf-panel-license-key" class="form-control form-control-lg" type="text" name="license_key" placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX">
-    </label>
-  </p>
+            $('#ctf-form').on('submit', function(evt){
 
-  <p>
-    <button class="button button-primary btn btn-primary" type="submit">
-		<?php echo $atts['form_button_label']; ?>
-    </button>
-  </p>
+                var button = $(this).find('.button');
 
-  <input type="hidden" name="action" value="np-ctf"> 
+                if(button.hasClass('is-loading')) {
+                    evt.preventDefault();
+                }
 
-	<?php wp_nonce_field('np-ctf-nonce'); ?>
+                button.addClass('is-loading');
+            })
+        });
+    })( jQuery );
+</script>
+
+<?php
+
+$plugins = array(
+    '2907' => 'Woo Floating Cart',
+    '2905' => 'Woo Quick View',
+    '2908' => 'Woo Variation Swatches'
+);
+
+$current_id = !empty($atts['id']) ? $atts['id'] : null;
+$current_plugin = !empty($current_id) ? $plugins[$current_id] : null;
+$messages = $this->messages->get_error_messages();
+$title = sprintf($atts['form_title'], !empty($current_plugin) ? '<span>'.$current_plugin.'</span>' : __('Your', 'np-ctf'), '<strong>CodeCanyon</strong>', '<strong>XplodedThemes</strong>');
+?>
+
+<h1 class="entry-title"><?php echo $title;?></h1>
+<form method="post" id="ctf-form" class="ctf-form<?php echo !empty($messages) ? ' ctf-form-shake': ''; ?>">
+
+    <?php do_action('ctf_success_message', $atts['form_success_message']); ?>
+
+    <?php if ($messages) : ?>
+        <div class="notification is-danger">
+            <button class="delete"></button>
+            <?php echo implode('<br>', $messages); ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="field">
+        <label class="label" for="np-ctf-panel-email"><?php echo $atts['form_id_label']; ?></label>
+        <div class="control">
+            <div class="select is-info">
+                <select id="xt-plugins" name="freemius_id">
+                    <option value="">-- Select Plugin --</option>
+                    <?php foreach($plugins as $id => $plugin): ?>
+                    <option <?php selected($current_id, $id);?> value="<?php echo $id;?>"><?php echo $plugin;?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <?php if(!empty($atts['id'])): ?>
+
+    <?php
+        $email = !empty($_POST['email']) ? $_POST['email'] : '';
+        $license_key = !empty($_POST['license_key']) ? $_POST['license_key'] : '';
+    ?>
+    <div class="field">
+        <label class="label" for="np-ctf-panel-email"><?php echo $atts['form_email_label']; ?></label>
+        <div class="control">
+            <input class="input is-info" type="email" name="email" value="<?php echo $email; ?>" placeholder="youremail@company.com" />
+        </div>
+    </div>
+
+    <div class="field">
+        <label class="label" for="np-ctf-panel-license-key"><?php echo $atts['form_license_label']; ?></label>
+        <div class="control">
+            <input class="input is-info" type="text" name="license_key" value="<?php echo $license_key; ?>" placeholder="XXXXXXXX-XXXXXXXX-XXXXXXXX-XXXXXXXX" />
+        </div>
+    </div>
+
+    <div class="field">
+        <div class="control">
+            <button class="button is-link" type="submit">
+                <?php echo $atts['form_button_label']; ?>
+            </button>
+        </div>
+    </div>
+
+    <input type="hidden" name="action" value="np-ctf" />
+
+    <?php wp_nonce_field('np-ctf-nonce'); ?>
+
+    <?php endif; ?>
 
 </form>
