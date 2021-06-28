9e21f5589df927c724a418b138652d9b68ba107d
Georges Haddad <prismosoft@gmail.com>
2021-06-28 17:47:02 -0400

    no message

--- views/success.php
+++ views/success.php
@@ -1,9 +1,10 @@
 <div class="notification is-success">
     <?php echo __('Your CodeCanyon License has been successfully migrated!'); ?>
 </div>
-<div>
-    <strong>
-        <?php echo __('Here is your new license key:');?>
-    </strong>
+<div class="notification is-info">
+    <strong><?php echo __('You will shortly receive an email with your account info and download link.'); ?></strong>
+</div>
+<div class="notification is-success">
+    <strong><?php echo __('Here is your new license key:');?></strong>
     <span class="license-key"><?php echo $key;?></span>
 </div>
