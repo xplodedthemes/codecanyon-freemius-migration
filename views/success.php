c697deee240593b553ffa854972a633339999527
Georges Haddad <prismosoft@gmail.com>
2021-06-28 17:05:12 -0400

    no message

--- views/success.php
+++ views/success.php
@@ -1,3 +1,9 @@
 <div class="notification is-success">
-    <?php echo sprintf(__('Your CodeCanyon License has been successfully migrated!<br>Here is your new License Key: <br>%s'), '<strong>'.$key.'</strong>'); ?>
+    <?php echo __('Your CodeCanyon License has been successfully migrated!'); ?>
+</div>
+<div>
+    <strong>
+        <?php echo __('Here is your new license key:');?>
+    </strong>
+    <span class="license-key"><?php echo $key;?>></span>
 </div>
